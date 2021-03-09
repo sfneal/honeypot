17e913bccc0e1583f734b59b05b0683a13bb4913
Stephen Neal <stephen.neal14@gmail.com>
2021-03-09 10:18:52 -0500

    MAKE TrackSpamQuery for retrieving recent TrackSpam records

---
+++ src/Queries/TrackSpamQuery.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace Sfneal\Honeypot\Queries;
+
+
+use Illuminate\Database\Eloquent\Builder;
+use Sfneal\Caching\Traits\Cacheable;
+use Sfneal\Honeypot\Models\TrackSpam;
+use Sfneal\Queries\Query;
+
+// todo: add tests
+class TrackSpamQuery extends Query
+{
+    use Cacheable;
+
+    /**
+     * @var int
+     */
+    private $limit;
+
+    /**
+     * TrackSpamQuery constructor.
+     *
+     * @param int $limit
+     */
+    public function __construct(int $limit = 200)
+    {
+        $this->limit = $limit;
+    }
+
+    /**
+     * Retrieve a Query builder.
+     *
+     * @return Builder
+     */
+    protected function builder(): Builder
+    {
+        return TrackSpam::query();
+    }
+
+    /**
+     * Execute a DB query.
+     *
+     * @return mixed
+     */
+    public function execute()
+    {
+        return $this->builder()
+            ->limit($this->limit)
+            ->get();
+    }
+
+    /**
+     * Retrieve the Query cache key.
+     *
+     * @return string
+     */
+    public function cacheKey(): string
+    {
+        return TrackSpam::getTableName() . ':recent#' . $this->limit;
+    }
+}
