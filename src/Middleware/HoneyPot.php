15fae2d25f6c707de3694ac31968bae85f5371db
Stephen Neal <stephen.neal14@gmail.com>
2021-05-05 13:06:07 -0400

    REFORMAT todo

--- src/Middleware/HoneyPot.php
+++ src/Middleware/HoneyPot.php
@@ -20,6 +20,7 @@ class HoneyPot extends ProtectAgainstSpam
     public function handle(Request $request, Closure $next): Response
     {
         // Check if first & last name inputs are the same (typical of spam)
+        // todo: add ability to change keys in config and/or with constructor
         if (
             $request->has('data.name_first')
             && $request->has('data.name_last')
