a307753284482decbc5025706863f4372db7233e
Stephen Neal <stephen.neal14@gmail.com>
2021-05-05 13:34:59 -0400

    CUT extending of root `TestCase` in spatie `TestCase` to avoid conflicts

--- tests/Spatie/TestCase.php
+++ tests/Spatie/TestCase.php
@@ -2,14 +2,30 @@
 
 namespace Sfneal\Honeypot\Tests\Spatie;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithContainer;
+use Illuminate\Support\Facades\View;
 use Sfneal\Honeypot\Tests\Spatie\TestClasses\FakeEncrypter;
+use Spatie\Honeypot\HoneypotServiceProvider;
 
-abstract class TestCase extends \Sfneal\Honeypot\Tests\TestCase
+abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
     use InteractsWithContainer;
 
     protected $testNow = true;
+
+    /**
+     * Register package service providers.
+     *
+     * @param Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            HoneypotServiceProvider::class
+        ];
+    }
 
     /**
      * Setup the test environment.
@@ -19,6 +35,8 @@ abstract class TestCase extends \Sfneal\Honeypot\Tests\TestCase
     public function setUp(): void
     {
         parent::setUp();
+
+        View::addLocation(__DIR__.'/../views');
 
         $this->swap('encrypter', new FakeEncrypter());
     }
