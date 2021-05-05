f272fdd89a75d31916dd38ed9ccffe60a20967a8
Stephen Neal <stephen.neal14@gmail.com>
2021-05-05 15:21:31 -0400

    ADD `track_spam_table_is_exists()` test method for confirming the table exists

--- tests/Feature/MigrationsTest.php
+++ tests/Feature/MigrationsTest.php
@@ -2,6 +2,7 @@
 
 namespace Sfneal\Honeypot\Tests\Feature;
 
+use Illuminate\Support\Facades\Schema;
 use Sfneal\Honeypot\Models\TrackSpam;
 use Sfneal\Honeypot\Tests\TestCase;
 use Sfneal\Testing\Utils\Traits\ModelAttributeAssertions;
@@ -9,6 +10,12 @@ use Sfneal\Testing\Utils\Traits\ModelAttributeAssertions;
 class MigrationsTest extends TestCase
 {
     use ModelAttributeAssertions;
+
+    /** @test  */
+    public function track_spam_table_is_exists()
+    {
+        $this->assertTrue(Schema::hasTable(TrackSpam::getTableName()));
+    }
 
     /** @test  */
     public function track_spam_table_is_accessible()
